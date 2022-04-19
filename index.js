7783f02cd2690f0ef7ce413723f2ed2eec2e856a
abdurrahmanff <abdurrahmanff15@gmail.com>
2022-04-19 17:35:47 +0700

    init project

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-const Discord = require('discord.js');
-const { token } = require('./config.json')
+const fs = require('node:fs');
+const { Client, Collection, Intents } = require('discord.js');
+const { token } = require('./config.json');
 
-const client = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 
-client.once('ready', () => {
-  console.log('online');
-});
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
-client.login(token)
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+
+}
